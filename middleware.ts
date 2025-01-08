9f2e0f61c6b29fc3a94237031e2fd18f7e82dc52
tungnt <thanhtung3523@gmail.com>
2025-01-09 01:29:03 +0700

    revert file middleware.ts before merge

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,8 @@
-// import { createMiddleware } from "@arcjet/next";
-// import aj from "./lib/arcjet";
+import { createMiddleware } from "@arcjet/next";
+import aj from "./lib/arcjet";
 
-// export const config = {
-//     matcher: ["/((?!_next/static|_next/image|favicon.ico|healthz).*)"],
-// }
-
-// export default createMiddleware(aj);
-
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL('/home', request.url))
-}
-
-// See "Matching Paths" below to learn more
 export const config = {
-    matcher: '/about/:path*',
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|healthz).*)"],
 }
+
+export default createMiddleware(aj);
