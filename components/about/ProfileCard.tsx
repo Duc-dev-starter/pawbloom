468aadd902a5dea685cc855a13abbf3dd9746c12
Duc-dev-starter <leminhduck@gmail.com>
2025-02-15 21:48:18 +0700

    fix to deploy pt3

--- components/about/ProfileCard.tsx
+++ components/about/ProfileCard.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
 import React from 'react'
 
 const ProfileCard = () => {
     return (
         <div className='grid grid-cols-3'>
             <div>
-                <Image className='rounded-2xl' />
             </div>
             <div className='flex flex-col gap-2'>
 
