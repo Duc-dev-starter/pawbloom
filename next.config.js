f956001c996f3f70cb0e24a986bb27ea96adcd0e
Duc-dev-starter <leminhduck@gmail.com>
2025-02-17 14:09:53 +0700

    fix to deploy

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'avatar.vercel.sh', 'loremflickr.com'],
   },
-  // experimental: {
-  //   runtime: 'nodejs', // Chạy trên Node.js runtime thay vì Edge
-  // },
+  experimental: {
+    runtime: 'nodejs', // Chạy trên Node.js runtime thay vì Edge
+  },
 };
 module.exports = nextConfig;
