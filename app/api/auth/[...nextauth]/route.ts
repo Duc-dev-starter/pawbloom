9cfab8798ccdb3e9d066cb058ca9106f8df0df90
Duc-dev-starter <leminhduck@gmail.com>
2025-03-04 10:50:30 +0700

    fix to deploy vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import Google from "next-auth/providers/google"
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     Google({
       clientId: process.env.NEXT_AUTH_GOOGLE_CLIENT_ID!,
@@ -10,23 +10,20 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
     async session({ session, token }) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
       session.accessToken = token.accessToken;
       return session;
     },
   },
 };
 
-// ✅ Cách export đúng cho App Router
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
