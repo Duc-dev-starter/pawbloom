905a88264d1f92e59e105e04f7821db02eaa60e2
Duc-dev-starter <leminhduck@gmail.com>
2025-03-04 11:33:45 +0700

    fix to deploy vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
-import NextAuth, { NextAuthOptions } from "next-auth"
-import Google from "next-auth/providers/google"
-
-export const authOptions: NextAuthOptions = {
+const handler = NextAuth({
   providers: [
     Google({
       clientId: process.env.NEXT_AUTH_GOOGLE_CLIENT_ID!,
@@ -17,13 +16,11 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async session({ session, token }) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-      session.accessToken = token.accessToken;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (session as any).accessToken = token.accessToken;
       return session;
     },
   },
-};
+});
 
-const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
